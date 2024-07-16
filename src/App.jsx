1252780bb78cf2dc291eab1aa593b7e313d136a8
Filipe Dias <126769503+FilipeDiass@users.noreply.github.com>
2024-07-16 01:06:45 +0000

    Adicionado font

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-xl font-bold underline">
+    <h1 className="text-xl font-bold underline font-inter">
       Hello world!
     </h1>
        
