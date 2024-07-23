32a4a2f5aee597cc91123d11655870358b429569
Alairton Junior <alairton20@gmail.com>
2024-07-22 21:16:03 -0300

    update: Fixed Sidebar

--- src/components/SidebarRight.jsx
+++ src/components/SidebarRight.jsx
@@ -8,7 +8,7 @@ import AddIcon from '/src/assets/icons/Add.jsx';
 
 const SidebarRight = () => {
   return (
-    <aside className=" right-0 top-0 h-full bg-[#F6F8FC] p-5 pt-24 flex flex-col items-center gap-10">
+    <aside className="fixed right-0 top-0 h-full bg-[#F6F8FC] p-5 pt-24 flex flex-col items-center gap-10">
       <CalendarIcon className="cursor-pointer"/>
       <LightBulbIcon className="cursor-pointer" />
       <TaskAltIcon className="cursor-pointer"/>
